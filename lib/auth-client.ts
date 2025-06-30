157381f490f6cc3dcc28347558a8edd0d2bc27c0
Had Code <decoding0506@gmail.com>
2025-06-30 14:23:48 +0700

    db connect maintenance

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,30 +1,7 @@
 import { createAuthClient } from "better-auth/react"
-// import { useRouter } from "next/navigation";
-// const router = useRouter()
 
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://pos-cashier-lovat.vercel.app/"
 })
 
-// await authClient.signOut({
-//   fetchOptions: {
-//     onSuccess: () => {
-//       router.push("/login");
-//     },
-//   },
-// });
-
 export const { signIn, signUp, useSession } = createAuthClient()
-
-// const { data, error } = await authClient.signUp.email({
-//   email: "test@example.com",
-//   password: "password1234",
-//   name: "test",
-//   image: "https://example.com/image.png",
-// });
-
-// const { data, error } = await authClient.signIn.email({
-//   email: "test@example.com",
-//   password: "password1234",
-// });
