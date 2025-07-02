31e8b38b740887511b0eb3457b0c8fcba25b0fb1
Had Code <decoding0506@gmail.com>
2025-07-02 18:10:21 +0700

    populate item by category

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,12 +1,19 @@
 import { connectToDatabase } from '@/lib/database';
-import { Item } from '@/lib/database/models/item.model';
+import ItemCategory from '@/lib/database/models/category.model';
+import { IItem, Item } from '@/lib/database/models/item.model';
+import { Query } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 import { NextResponse } from 'next/server';
 
+const populateItem = (query: Query<HydratedDocument<IItem>[], HydratedDocument<IItem>>) => {
+  return query
+  .populate({ path: 'category', model: ItemCategory, select: '_id' })
+}
 // GET: Get all items
 export async function GET() {
   try {
     await connectToDatabase();
-    const items = await Item.find();
+    const items = await populateItem(Item.find());
     return NextResponse.json(items);
   } catch (error) {
     return NextResponse.json({ message: "Failed to fetch items", error }, { status: 500 });
